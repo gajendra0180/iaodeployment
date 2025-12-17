9c1cc356b84b4424a41f10eb313082115fe78add
gajendra-0180 <gajendrahitz@gmail.com>
2025-12-17 09:46:57 +0000

    fix build

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // Vercel serverless function wrapper for Express app
-import app from '../dist/src/index.js';
+import app from './dist/src/index.js';
 
 // Vercel serverless functions expect a default export
 export default app;
